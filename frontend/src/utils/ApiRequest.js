90992258c63dd206bf591e0bf750d8a96ebecd43
priyansh0401 <priyansh0401@icloud.com>
2024-05-06 07:47:46 +0530

    a

--- frontend/src/utils/ApiRequest.js
+++ frontend/src/utils/ApiRequest.js
@@ -1,4 +1,4 @@
-const host = "management-server-red.vercel.app";
+const host = "https://management-server-red.vercel.app/";
 // const host = "http://localhost:5000";
 export const setAvatarAPI = `${host}/api/auth/setAvatar`;
 export const registerAPI = `${host}/api/auth/register`;
