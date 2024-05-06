42d9710d46e2de0486a133cc6f59c3a6873a53b8
priyansh0401 <priyansh0401@icloud.com>
2024-05-06 07:43:35 +0530

    a

--- backend/api/index.js
+++ backend/api/index.js
@@ -19,6 +19,7 @@ connectDB();
 const allowedOrigins = [
   "https://main.d1sj7cd70hlter.amplifyapp.com",
   "https://expense-tracker-app-three-beryl.vercel.app",
+  "www.moneyminds.tech"
   // add more origins as needed
 ];
 
