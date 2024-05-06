9341e6fab71c83b5a182f3d3a175101963f1b8d6
priyansh0401 <priyansh0401@icloud.com>
2024-05-06 20:36:48 +0530

    a

--- frontend/src/utils/ApiRequest.js
+++ frontend/src/utils/ApiRequest.js
@@ -1,5 +1,5 @@
 // const host = "https://expense-tracker-app-knl1.onrender.com";
-const host = "https://management-backend-pearl.vercel.app/";
+const host = "https://management-backend-pearl.vercel.app";
 export const setAvatarAPI = `${host}/api/auth/setAvatar`;
 export const registerAPI = `${host}/api/auth/register`;
 export const loginAPI = `${host}/api/auth/login`;
